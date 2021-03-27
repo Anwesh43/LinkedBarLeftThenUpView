1591f9baffb7d7e3c7f843b907e01b0388f329b3
Anwesh43 <anweshthecool0@gmail.com>
2021-03-27 16:47:03 +0530

    adding Renderer class

--- BarLeftThenUpView/src/main/java/com/example/barleftthenupview/BarThenLeftUpView.kt
+++ BarLeftThenUpView/src/main/java/com/example/barleftthenupview/BarThenLeftUpView.kt
@@ -201,4 +201,27 @@ class BarThenLeftUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BarThenLeftUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val bltu : BarThenLeftUp = BarThenLeftUp(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bltu.draw(canvas, paint)
+            animator.animate {
+                bltu.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bltu.startUdpating {
+                animator.start()
+            }
+        }
+    }
 }
